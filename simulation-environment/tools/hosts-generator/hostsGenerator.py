afb4212c4035728787b08ebb28f248a63cf02afe
ofen_an <annika.ofenloch@dlr.de>
2017-08-14 14:55:05 +0200

    Add script to create hosts-file from model-config.xml

---
+++ simulation-environment/tools/hosts-generator/hostsGenerator.py
@@ -0,0 +1,56 @@
+'''
+Created on Aug 14, 2017
+
+@author: Annika Ofenloch
+'''
+
+from lxml import etree as ET
+import configparser
+import argparse
+from test.support import args_from_interpreter_flags
+
+
+class InventoryWriter():
+    def __init__(self):
+        self.config = configparser.ConfigParser(allow_no_value=True)
+        pass
+
+    def write_hosts_to_ini(self, root):
+        
+        self.config['MASTER'] = {}
+        for host in root.iter('Localhost'):
+            localhostname = host.find("Name").text
+            self.config['MASTER']["{0} {1}".format(localhostname, 'ansible_host=localhost')] = None 
+           
+        self.config["SLAVES"] = {}
+        for host in root.iter('Host'):
+            hostname = host.find("Name").text
+            hostaddress = host.find("Address").text
+        
+            self.config["SLAVES"]["{0} {1}".format(hostname,"ansible_host={0}".format(hostaddress))] = None
+
+    def get_configuration(self):
+        return self.config
+
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser(description='Process some integers.')
+    args_parser.add_argument('-f', '--file' , dest='file', help="Add xml-file of models-configuration", required=True)
+    args_parser.add_argument('-d', '--dest' , dest='destination', help="Add path for 'hosts'-file", required=True)
+
+    args = args_parser.parse_args()
+
+    inv_writer = InventoryWriter()
+    filename = args.file
+
+    tree = ET.ElementTree()
+    tree.parse(filename)
+    root = tree.getroot()
+    
+    inv_writer.write_hosts_to_ini(root);
+    
+    with open("{0}/hosts".format(args.destination), 'w') as configfile:
+        inv_writer.get_configuration().write(configfile)
+
+    print("File 'hosts.ini' was created.")
+
+
