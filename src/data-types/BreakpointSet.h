edea587ccc792bf190c5a0b473c4c68278820e9b
Annika Ofenloch <annika.ofenloch@dlr.de>
2017-08-21 15:32:25 +0200

    Add data-types

---
+++ src/data-types/BreakpointSet.h
@@ -0,0 +1,52 @@
+/*
+ * BreakpointSet.h
+ *
+ *  Created on: Mar 17, 2017
+ *      Author: Annika Ofenloch
+ */
+
+#ifndef EUCROPIS_SIM_TRUNK_SRC_DATA_TYPES_BREAKPOINTSET_H_
+#define EUCROPIS_SIM_TRUNK_SRC_DATA_TYPES_BREAKPOINTSET_H_
+
+#include <vector>
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/version.hpp>
+
+class BreakpointSet: public std::vector<int> {
+public:
+	BreakpointSet() :
+		mSize(0) {
+	}
+
+	virtual ~BreakpointSet() {
+	}
+
+private:
+	friend class boost::serialization::access;
+
+	template<class Archive>
+	void serialize(Archive &ar, const unsigned int) {
+		if (Archive::is_loading::value) {
+			this->clear();
+			ar & boost::serialization::make_nvp("Size", mSize);
+			for (int i = 0; i < mSize; i++) {
+				int breakpoint;
+				ar & boost::serialization::make_nvp("Breakpoint", breakpoint);
+				this->push_back(breakpoint);
+			}
+		} else {
+			mSize = this->size();
+			ar & boost::serialization::make_nvp("Size", mSize);
+			for (int i = 0; i < mSize; i++) {
+				ar & boost::serialization::make_nvp("Breakpoint", this->at(i));
+			}
+		}
+	}
+
+	int mSize; // You have to add vector size
+};
+
+//BOOST_CLASS_VERSION(EventSet, 0);
+//BOOST_CLASS_IMPLEMENTATION(EventSet, boost::serialization::object_serializable)
+
+#endif /* EUCROPIS_SIM_TRUNK_SRC_DATA_TYPES_BREAKPOINTSET_H_ */
